36bf6a0c5e8c6eba9833b1adbc1e80a08038a6bc
Tan Yuanhong <leotan.yh@gmail.com>
2019-02-12 23:06:52 +0800

    minor improvements on CLI

--- Amazons.java
+++ Amazons.java
@@ -23,58 +23,73 @@ public class Amazons {
 
     public static void main(String args[]) {
         // AmazonsGUI gui = new AmazonsGUI();
-        // TODO: implement the proof-of-concept CLI interface
         ChessBoard cb = new ChessBoard();
         Scanner sc = new Scanner(System.in);
         boolean ok = false;
 
-        // choose the number of players
-        System.out.println("Please choose the number of players.");
-        System.out.println("1:One player mode(play with the computer).");
-        System.out.println("2:Two player mode.");
-        int playerNumber = sc.nextInt();
+        while (!ok) {
 
-        // TODO:one player mode
+            // choose the number of players
+            System.out.println("Please choose the number of players.");
+            System.out.println("1: One player mode (play with the computer).");
+            System.out.println("2: Two player mode.");
+            // TODO: This may also lead to illegal input, which can be resolved in similar
+            // ways as the upcoming nextInt()'s
+            int playerNumber = sc.nextInt();
 
-        // two player mode
-        if (playerNumber == 2) {
-            cb.printBoard();
-            System.out.println("0 2 3 3 3 4 means \nmove the chess on (0,2) to (3,3) \nand place an obstacle on (3,4)\n");
-            System.out.printf("It is turn for %s.", cb.colorForTurn() == 0 ? "+" : "-");
-            do {
+            if (playerNumber == 1) {
+                // TODO: One player mode
                 ok = true;
-                // clrscr(); // I dunno why this doesn' work...
-                int x = sc.nextInt();
-                int y = sc.nextInt();
-                int xx = sc.nextInt();
-                int yy = sc.nextInt();
-                int xxx = sc.nextInt();
-                int yyy = sc.nextInt();
-                // if(cb.isLegalMove(x, y, xx, yy, xxx, yyy)) {
-                // List<Pair> possible = cb.getPiece(x, y).possiblePositions(cb);
-                // System.out.println("Possible positions:");
-                // }
-                // for (Pair p : possible) {
-                // System.out.printf("(%d, %d) ", p.x, p.y);
-                // }
-                System.out.println();
-                if (!cb.movePiece(x, y, xx, yy, xxx, yyy)) {
-                    System.out.println("Illegal input. Please try again.");
-                    ok = false;
-                    continue;
-                }
+            } else if (playerNumber == 2) {
+                // Two player mode
+                ok = true;
                 cb.printBoard();
-                int checkResult=cb.declareResult();
-                if(checkResult==1) {
-                    System.out.println("Game over, the white wins.");
-                    break;
-                }
-                if(checkResult==0) {
-                    System.out.println("Game over, the black wins.");
-                    break;
-                }
+                System.out.println(
+                        "0 2 3 3 3 4 means \nmove the chess on (0,2) to (3,3) \nand place an obstacle on (3,4)\n");
                 System.out.printf("It is turn for %s.", cb.colorForTurn() == 0 ? "+" : "-");
-            } while (sc.hasNext() || !ok);
+                do {
+                    int x, y, xx, yy, xxx, yyy;
+                    try {
+                        ok = true;
+                        // clrscr(); // I dunno why this doesn' work...
+                        x = sc.nextInt();
+                        y = sc.nextInt();
+                        xx = sc.nextInt();
+                        yy = sc.nextInt();
+                        xxx = sc.nextInt();
+                        yyy = sc.nextInt();
+                        // if(cb.isLegalMove(x, y, xx, yy, xxx, yyy)) {
+                        // List<Pair> possible = cb.getPiece(x, y).possiblePositions(cb);
+                        // System.out.println("Possible positions:");
+                        // }
+                        // for (Pair p : possible) {
+                        // System.out.printf("(%d, %d) ", p.x, p.y);
+                        // }
+                    } catch (InputMismatchException e) {
+                        sc.next();
+                        System.out.println("Illegal input. Please try again.");
+                        continue;
+                    }
+                    System.out.println();
+                    if (!cb.movePiece(x, y, xx, yy, xxx, yyy)) {
+                        System.out.println("Illegal input. Please try again.");
+                        ok = false;
+                        continue;
+                    }
+                    cb.printBoard();
+                    int checkResult = cb.declareResult();
+                    if (checkResult == 1) {
+                        System.out.println("Game over, the white wins.");
+                        break;
+                    }
+                    if (checkResult == 0) {
+                        System.out.println("Game over, the black wins.");
+                        break;
+                    }
+                    System.out.printf("It is turn for %s.\n", cb.colorForTurn() == 0 ? "+" : "-");
+                } while (sc.hasNext() || !ok);
+            }
         }
     }
+
 }
